4cbb898e68501ef2fe5121d5ef1cf74c09ab5fd0
Tehaeka <167438862+Tehaeka@users.noreply.github.com>
2025-11-03 00:22:15 +0100

    Create zapis_zaufanych_mac.py

---
+++ zapis_zaufanych_mac.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+device = {
+    'device_type': 'linux',
+    'host':   '192.168.5.82',
+    'username': 'ubuntu',
+    'password': 'linux123',
+}
+mac=[]
+connection = ConnectHandler(**device)
+output = connection.send_command('sudo nmap -sn -PR 192.168.5.0/24')
+connection.disconnect()
+MACs = output.splitlines()
+print(output)
+mac =[linie.split()[2] for linie in MACs if "MAC Address" in linie]
+mac=set(mac)
+mac=list(mac)
+with open('mac.txt', 'w+') as file:
+    for linie in mac:
+        file.write(linie + "\n")
